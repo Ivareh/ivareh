343d6a00d37c556e8831d4af6ca458a676107457
Ivareh <ivareh@ntnu.no>
2025-02-07 08:07:22 +0100

    #1 Created a template to work from

---
+++ backend/app/utils.py
@@ -0,0 +1,23 @@
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
+
+from jinja2 import Template
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class EmailData:
+    html_content: str
+    subject: str
+
+
+def render_email_template(*, template_name: str, context: dict[str, Any]) -> str:
+    template_str = (
+        Path(__file__).parent / "email-templates" / "build" / template_name
+    ).read_text()
+    html_content = Template(template_str).render(context)
+    return html_content
---
+++ frontend/src/routes/_layout/index.tsx
@@ -0,0 +1,20 @@
+import { Box, Container, Text } from "@chakra-ui/react"
+import { createFileRoute } from "@tanstack/react-router"
+
+export const Route = createFileRoute("/_layout/")({
+  component: Dashboard,
+})
+
+function Dashboard() {
+  return (
+    <>
+      <Container maxW="full">
+        <Box pt={12} m={4}>
+          <Text fontSize="2xl">
+          </Text>
+          <Text>Welcome to Ivar's dev page!</Text>
+        </Box>
+      </Container>
+    </>
+  )
+}
