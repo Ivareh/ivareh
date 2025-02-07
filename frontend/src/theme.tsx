002c2401fdc280fbc11881f6f3e8f5ea3c88ac73
Ivareh <ivareh@ntnu.no>
2025-02-07 12:17:06 +0100

    Added coloring fit for a console

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -1,59 +1,14 @@
 import { extendTheme } from "@chakra-ui/react"
 
-const disabledStyles = {
-  _disabled: {
-    backgroundColor: "ui.main",
-  },
-}
-
 const theme = extendTheme({
   colors: {
     ui: {
-      main: "#009688",
-      secondary: "#EDF2F7",
-      success: "#48BB78",
-      danger: "#E53E3E",
-      light: "#FAFAFA",
-      dark: "#1A202C",
-      darkSlate: "#252D3D",
-      dim: "#A0AEC0",
-    },
-  },
-  components: {
-    Button: {
-      variants: {
-        primary: {
-          backgroundColor: "ui.main",
-          color: "ui.light",
-          _hover: {
-            backgroundColor: "#00766C",
-          },
-          _disabled: {
-            ...disabledStyles,
-            _hover: {
-              ...disabledStyles,
-            },
-          },
-        },
-        danger: {
-          backgroundColor: "ui.danger",
-          color: "ui.light",
-          _hover: {
-            backgroundColor: "#E32727",
-          },
-        },
-      },
-    },
-    Tabs: {
-      variants: {
-        enclosed: {
-          tab: {
-            _selected: {
-              color: "ui.main",
-            },
-          },
-        },
-      },
+      main: "#222727",
+      plain: "#e8e9e9",
+      blueOutput: "#1e91e2",
+      purplearrow: "#897abb",
+      success: "#17b21a",
+      error: "#b9392c",
     },
   },
 })
