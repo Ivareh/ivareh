f2817e55feae5ffd41cc76359e61577f1d864e70
Ivareh <ivareh@ntnu.no>
2025-02-21 10:22:46 +0100

    Added image blob container and removed start.sh

--- blob-storage/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Run initialization
-python init_azurite.py --directory=init_containers
-
-# Start Azurite blob service (replace with the correct command if needed)
-exec azurite-blob --blobHost 0.0.0.0 --blobPort 10000
