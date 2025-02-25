9c85c82b3fa5ae81a9685c49b064c6e5a64c4893
Ivareh <ivareh@ntnu.no>
2025-02-25 07:45:19 +0100

    Added image router to main api

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 from app.api.routes import images, utils
 
 api_router = APIRouter()
-api_router.include_router(images.image_router)
+api_router.include_router(images.router)
 api_router.include_router(utils.router)
